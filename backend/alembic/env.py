4cc301125fc3753be35959df3098b085de250967
ReeceJones <reece_jones@icloud.com>
2024-09-01 21:00:35 -0500

    Update backend to use image pull secrets stored as json

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,20 +1,18 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
+from app.config import CONFIG
+from app.models import Base
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from alembic import context
-
-from app.models import Base
-from app.config import CONFIG
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option('sqlalchemy.url', CONFIG.DB_URI)
+config.set_main_option("sqlalchemy.url", CONFIG.DB_URI.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
